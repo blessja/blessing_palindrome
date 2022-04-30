d945aa9778fc347a9b05c4ab5a40b3dffd0f5c22
blessja <jaydenj772@gmail.com>
2022-04-30 11:51:18 +0200

    Factor into module, add integer palindrome detection

--- lib/blessing_palindrome.rb
+++ lib/blessing_palindrome.rb
@@ -1,25 +1,30 @@
 # frozen_string_literal: true
 
 require_relative "blessing_palindrome/version"
-
-class String
-    
+module BlessingPalindrome
     # Returns true for a palindrome, false otherwise.
       def palindrome?
-        processed_content == processed_content.reverse
+      processed_content == processed_content.reverse
       end
 
-      # Returns the letters in the String.
-      # def letters
-      #   self.chars.select { |c| c.match(/[a-z]/i)}.join
-      # end
-    
+    # Returns the letters in the String.
+    # def letters
+    #   self.chars.select { |c| c.match(/[a-z]/i)}.join
+    # end
+
     private
 
-        #Returns content for palindrome testing.
-        def processed_content
-            self.scan(/[a-z]/i).join.downcase
-        end
-  
-     
+      #Returns content for palindrome testing.
+      def processed_content
+          self.to_s.scan(/[a-z\d]/i).join.downcase
+      end
+
+end
+
+class String
+    include BlessingPalindrome 
+end
+
+class Integer
+  include BlessingPalindrome 
 end
