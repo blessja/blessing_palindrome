bdc0b44c70369ae659f9c3e62e30bb3760476bbb
blessja <jaydenj772@gmail.com>
2022-04-30 09:05:56 +0200

    Finish working and refactored palindrome method

--- lib/blessing_palindrome.rb
+++ lib/blessing_palindrome.rb
@@ -2,7 +2,24 @@
 
 require_relative "blessing_palindrome/version"
 
-module BlessingPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+    
+    # Returns true for a palindrome, false otherwise.
+      def palindrome?
+        processed_content == processed_content.reverse
+      end
+
+      # Returns the letters in the String.
+      # def letters
+      #   self.chars.select { |c| c.match(/[a-z]/i)}.join
+      # end
+    
+    private
+
+        #Returns content for palindrome testing.
+        def processed_content
+            self.scan(/[a-z]/i).join.downcase
+        end
+  
+     
 end
--- test/blessing_palindrome_test.rb
+++ test/blessing_palindrome_test.rb
@@ -3,11 +3,24 @@
 require "test_helper"
 
 class BlessingPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::BlessingPalindrome::VERSION
+ 
+  def test_for_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_for_literal_palindrome
+    assert "racecar".palindrome?
   end
+
+  def test_mixed_case
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  # def test_letters
+  #   assert_equal "MadamImAdam","Madam, I'm Adam.".letters, "MadamImAdam"
+  # end
 end
